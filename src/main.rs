3e18ad6314c4b15d62b67d1a4991aa073b138315
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-08-26 21:47:34 +0300

    Start transition to pest

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 mod ast;
-mod grammar;
 mod types;
 mod value;
 mod vm;
 
 use std::fs::File;
 use std::io::{self, Read};
+use pest::Parser;
 
 use value::Value;
+
+const _GRAMMAR: &str = include_str!("grammar.pest");
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct RuoParser;
 
 fn print(value: Value) -> Value {
     println!("{:?}", value);
@@ -15,44 +25,19 @@ fn print(value: Value) -> Value {
 }
 
 fn main() -> io::Result<()> {
-    let mut file = File::open("scripts/arithmetic.ruo")?;
-    let parser = grammar::ProgramParser::new();
     let mut input = String::new();
-    file.read_to_string(&mut input)?;
-    let ast = parser.parse(&input).unwrap();
+    let mut file = File::open("scripts/arithmetic.ruo")?.read_to_string(&mut input)?;
 
-    println!("Ast: {:?}", ast);
+    let ast = RuoParser::parse(Rule::program, &input).unwrap();
+    println!("Ast: {:#?}", ast);
+    /*println!("Ast: {:?}", ast);
 
     let mut vm = vm::VM::new();
     vm.add_variable("print", Value::Function(print));
     vm.parse_ast(ast);
     vm.execute();
 
-    println!("Variables: {:?}", vm.variables());
+    println!("Variables: {:?}", vm.variables());*/
 
     Ok(())
 }
-
-/*
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let mut vm = vm::VM::new();
-    for input in stdin.lock().lines() {
-        vm.clear_commands();
-        let input = input.unwrap();
-        if input == "halt".to_string() {
-            break;
-        }
-
-        let parser = grammar::ExprParser::new();
-        let ast = parser.parse(&input).unwrap();
-        println!("{:?}", ast);
-        vm.parse_ast(ast);
-        let _ = vm.execute();
-        let variables = vm.variables();
-        println!("Variables: {:?}", variables);
-    }
-
-    Ok(())
-}
-*/
