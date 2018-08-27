74b15f3182a1d476f91f2a89fd4897c520d4f4f4
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-08-28 00:36:49 +0300

    Replace pest::prec_climber with custom solution

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ mod types;
 mod value;
 mod vm;
 
-use pest::prec_climber::{Assoc, Operator, PrecClimber};
 use pest::{
     iterators::{Pair, Pairs},
-    Parser,
+    Parser as PestParser
 };
+
+use prec_climber::{Parser, tokenize};
 use std::fs::File;
 use std::io::{self, Read};
 use std::str::FromStr;
@@ -32,35 +33,6 @@ fn print(value: Value) -> Value {
     Value::Empty
 }
 
-impl Rule {
-    pub fn to_bin_op(&self) -> Option<BinOp> {
-        match *self {
-            Rule::op_add => Some(BinOp::Add),
-            Rule::op_sub => Some(BinOp::Sub),
-            Rule::op_mul => Some(BinOp::Mul),
-            Rule::op_div => Some(BinOp::Div),
-            Rule::op_pow => Some(BinOp::Pow),
-            _ => None,
-        }
-    }
-}
-
-fn prec_climber() -> PrecClimber<Rule> {
-    PrecClimber::new(vec![
-        Operator::new(Rule::op_or, Assoc::Left),
-        Operator::new(Rule::op_and, Assoc::Left),
-        Operator::new(Rule::op_eq, Assoc::Left)
-            | Operator::new(Rule::op_neq, Assoc::Left)
-            | Operator::new(Rule::op_ge, Assoc::Left)
-            | Operator::new(Rule::op_gt, Assoc::Left)
-            | Operator::new(Rule::op_le, Assoc::Left)
-            | Operator::new(Rule::op_lt, Assoc::Left),
-        Operator::new(Rule::op_add, Assoc::Left) | Operator::new(Rule::op_sub, Assoc::Left),
-        Operator::new(Rule::op_mul, Assoc::Left) | Operator::new(Rule::op_div, Assoc::Left),
-        Operator::new(Rule::op_pow, Assoc::Right),
-    ])
-}
-
 fn main() -> io::Result<()> {
     let mut input = String::new();
     let mut file = File::open("scripts/arithmetic.ruo")?.read_to_string(&mut input)?;
@@ -71,26 +43,13 @@ fn main() -> io::Result<()> {
         .unwrap();
     println!("Ast: {:#?}", ast);
 
-    fn climb(expr: Pairs<Rule>) -> Box<Expr> {
-        let primary = |pair: Pair<Rule>| match pair.as_rule() {
-            Rule::int => Box::new(Expr::Int(FromStr::from_str(pair.as_str()).unwrap())),
-            Rule::float => Box::new(Expr::Float(FromStr::from_str(pair.as_str()).unwrap())),
-            Rule::identifier => Box::new(Expr::Variable(pair.as_str().to_string())),
-            Rule::bin_expr => climb(pair.into_inner()),
-            _ => unreachable!(),
-        };
+    let tokens = tokenize(ast.into_inner().next().unwrap());
 
-        let infix = |lhs: Box<Expr>, op: Pair<Rule>, rhs: Box<Expr>| {
-            if let Some(bin_op) = op.as_rule().to_bin_op() {
-                Box::new(Expr::BinOp(lhs, bin_op, rhs))
-            } else {
-                panic!("not binop!");
-            }
-        };
-        prec_climber().climb(expr, primary, infix)
-    }
+    let mut parser = Parser::new(tokens.iter());
+    let expr = parser.expression(0);
 
-    println!("{:#?}", climb(ast.into_inner()));
+    println!("{:#?}", expr);
+
 
     /*println!("Ast: {:?}", ast);
 
--- src/prec_climber.rs
+++ src/prec_climber.rs
@@ -1,162 +1,93 @@
-// Modified from pest project
-// Copyright (c) 2018 Dragoș Tiselice
-//
-// Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
-// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. All files in the project carrying such notice may not be copied,
-// modified, or distributed except according to those terms.
+use ast::{Expr, BinOp, UnOp};
+use pest::iterators::{Pairs, Pair};
+use Rule;
 
-use std::collections::HashMap;
 use std::iter::Peekable;
-use std::ops::BitOr;
-
-use pest::iterators::Pair;
-use pest::RuleType;
-
-/// An `enum` describing an `Operator`'s associativity.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum Assoc {
-    /// Left `Operator` associativity
-    Left,
-    /// Right `Operator` associativity
-    Right,
-}
+use std::slice::Iter;
 
 #[derive(Debug)]
-pub enum OperatorType {
-    Infix { assoc: Assoc },
-    Prefix,
+pub enum Token {
+    Add,
+    Mul,
+    Int(i64),
 }
 
-#[derive(Debug)]
-pub struct Operator<R: RuleType> {
-    op_type: OperatorType,
-    rule: R,
-    next: Option<Box<Operator<R>>>,
+pub fn tokenize(pair: Pair<Rule>) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    if let Rule::bin_expr = pair.as_rule() {
+        for pair in pair.into_inner() {
+            let token = match pair.as_rule() {
+                Rule::int => Token::Int(pair.as_str().parse().unwrap()),
+                Rule::op_add => Token::Add,
+                Rule::op_mul => Token::Mul,
+                _ => unimplemented!()
+            };
+            tokens.push(token);
+        }
+    } else {
+        panic!("Not a binary expression");
+    }
+    tokens
 }
 
-impl<R: RuleType> Operator<R> {
-    pub fn new_infix(rule: R, assoc: Assoc) -> Operator<R> {
-        Operator {
-            op_type: OperatorType::Infix { assoc },
-            rule,
-            next: None,
+impl Token {
+    pub fn lbp(&self) -> u32 {
+        match *self {
+            Token::Mul => 20,
+            Token::Add => 10,
+            _ => 0,
         }
     }
 
-    pub fn new_prefix(rule: R) -> Operator<R> {
-        Operator {
-            op_type: OperatorType::Prefix,
-            rule,
-            next: None,
+    pub fn nud(&self) -> Box<Expr> {
+        match *self {
+            Token::Int(i) => Box::new(Expr::Int(i)),
+            _ => panic!("Not a int"),
         }
     }
-}
 
-impl<R: RuleType> BitOr for Operator<R> {
-    type Output = Self;
-
-    fn bitor(mut self, rhs: Self) -> Self {
-        fn assign_next<R: RuleType>(op: &mut Operator<R>, next: Operator<R>) {
-            if let Some(ref mut child) = op.next {
-                assign_next(child, next);
-            } else {
-                op.next = Some(Box::new(next));
+    pub fn led(&self, parser: &mut Parser, lhs: Box<Expr>) -> Box<Expr> {
+        match *self {
+            Token::Add | Token::Mul => {
+                let rhs = parser.expression(self.lbp());
+                let op = match *self {
+                    Token::Add => BinOp::Add,
+                    Token::Mul => BinOp::Mul,
+                    _ => unreachable!()
+                };
+                Box::new(Expr::BinOp(lhs, op, rhs))
             }
+            _ => unreachable!()
         }
-
-        assign_next(&mut self, rhs);
-        self
     }
 }
 
-#[derive(Debug)]
-pub struct PrecClimber<R: RuleType> {
-    ops: HashMap<R, (u32, Option<Assoc>)>,
+pub struct Parser<'a> {
+    tokens: Peekable<Iter<'a, Token>>,
 }
 
-impl<R: RuleType> PrecClimber<R> {
-    pub fn new(ops: Vec<Operator<R>>) -> PrecClimber<R> {
-        let ops = ops
-            .into_iter()
-            .zip(1..)
-            .fold(HashMap::new(), |mut map, (op, precedence)| {
-                let mut next = Some(op);
-
-                while let Some(op) = next.take() {
-                    let assoc = if let OperatorType::Infix { assoc } = op.op_type { Some(assoc) } else { None };
-                    map.insert(op.rule, (precedence, assoc));
-                    next = op.next.map(|op| *op);
-                }
-
-                map
-            });
-
-        PrecClimber { ops }
+impl<'a> Parser<'a> {
+    pub fn new(tokens: Iter<'a, Token>) -> Self {
+        Self { tokens: tokens.peekable() }
     }
 
-    pub fn climb<'i, P, F, G, T>(&self, mut pairs: P, mut primary: F, mut infix: G) -> T
-    where
-        P: Iterator<Item = Pair<'i, R>>,
-        F: FnMut(Pair<'i, R>) -> T,
-        G: FnMut(T, Pair<'i, R>, Option<T>) -> T,
-    {
-        let lhs = primary(
-            pairs
-                .next()
-                .expect("precedence climbing requires a non-empty Pairs"),
-        );
-        self.climb_rec(lhs, 0, &mut pairs.peekable(), &mut primary, &mut infix)
+    pub fn next_binds_tighter_than(&mut self, rbp: u32) -> bool {
+        self.tokens.peek().map_or(false, |t| { t.lbp() > rbp })
     }
 
-    fn climb_rec<'i, P, F, G, T>(
-        &self,
-        mut lhs: T,
-        min_precedence: u32,
-        pairs: &mut Peekable<P>,
-        primary: &mut F,
-        infix: &mut G,
-    ) -> T
-    where
-        P: Iterator<Item = Pair<'i, R>>,
-        F: FnMut(Pair<'i, R>) -> T,
-        G: FnMut(T, Pair<'i, R>, Option<T>) -> T,
-    {
-        while pairs.peek().is_some() {
-            let rule = pairs.peek().unwrap().as_rule();
-            if let Some(&(precedence, _)) = self.ops.get(&rule) {
-                if precedence >= min_precedence {
-                    let op = pairs.next().unwrap();
-                    let mut rhs = primary(pairs.next().expect(
-                        "infix operator must be followed by \
-                         a primary expression",
-                    ));
+    pub fn parse_nud(&mut self) -> Box<Expr> {
+        self.tokens.next().unwrap().nud()
+    }
 
-                    while pairs.peek().is_some() {
-                        let rule = pairs.peek().unwrap().as_rule();
-                        if let Some(&(new_precedence, assoc)) = self.ops.get(&rule) {
-                            if new_precedence > precedence
-                                || assoc == Assoc::Right && new_precedence == precedence
-                            {
-                                rhs = self.climb_rec(rhs, new_precedence, pairs, primary, infix);
-                            } else {
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    }
+    pub fn parse_led(&mut self, expr: Box<Expr>) -> Box<Expr> {
+        self.tokens.next().unwrap().led(self, expr)
+    }
 
-                    lhs = infix(lhs, op, rhs);
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
+    pub fn expression(&mut self, rbp: u32) -> Box<Expr> {
+        let mut left = self.parse_nud();
+        while self.next_binds_tighter_than(rbp) {
+            left = self.parse_led(left);
         }
-
-        lhs
+        left
     }
 }
